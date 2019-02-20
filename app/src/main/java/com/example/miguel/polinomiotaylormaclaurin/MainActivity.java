3e3cf61db73e2f5a2d8fcaec134d023328d8072c
MiguelLPmez <mig.angel.1916@gmail.com>
2019-02-19 19:18:43 -0600

    Initial version

--- app/src/main/java/com/example/miguel/polinomiotaylormaclaurin/MainActivity.java
+++ app/src/main/java/com/example/miguel/polinomiotaylormaclaurin/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // - Este es un comentario.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
