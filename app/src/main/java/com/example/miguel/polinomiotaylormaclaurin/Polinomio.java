501e7345d46267cc2d73c43ac802565fd34a401a
Jurgen <jurgen.gonzalezpadilla@outlook.com>
2019-02-19 21:11:44 -0600

    por Jurgen enum agregado

--- app/src/main/java/com/example/miguel/polinomiotaylormaclaurin/Polinomio.java
+++ app/src/main/java/com/example/miguel/polinomiotaylormaclaurin/Polinomio.java
@@ -2,17 +2,39 @@ package com.example.miguel.polinomiotaylormaclaurin;
 
 public class Polinomio {
 
-    private int n, x, resultado, derivada;
+    private int n, x, resultado, derivada, resultadoDerivada;
 
-    private void polinomios(){
+    private enum Funcion{
+        COSENO,
+        SENO,
+        EULER
+    }
+
+    private void polinomios(Funcion funcion){
+        resultado = 0;
         for(int i = 0; i < n; i++){
-            resultado += derivada * Math.pow(x, i) / factorial(i);
+            resultado += derivada(funcion) * Math.pow(x, i) / factorial(i);
         }
+    }
+
+    private int derivada(Funcion funcion){
+
+        switch(funcion){
+            case SENO:
+                break;
+            case COSENO:
+                break;
+            case EULER:
+                resultadoDerivada = 1;
+                break;
+            default: assert funcion != null;
+        }
+        return resultadoDerivada;
     }
 
     private int factorial(int numero){
         if(numero == 0) return 1;
-        else return factorial(numero-1);
+        else return numero * factorial(numero-1);
     }
 
 
